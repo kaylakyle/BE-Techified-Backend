b83df3e500f7b161d3b9d20ec974d568e00eb7c6
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-02-14 17:41:04 +0300

    Week 1 Be Techiefied Backend

---
+++ Week 1/intro.js
@@ -0,0 +1,7 @@
+const name = "Lynn Kati";  
+const age = 22;  
+const favouriteLanguages = ["JavaScript", "Python", "GOlang"];  
+
+console.log("Name:", name);  
+console.log("Age:", age);  
+console.log("Favourite Programming Languages:", favouriteLanguages.join(", "));
